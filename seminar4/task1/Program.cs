577e432004be06b45fbabf3b203940577a359c67
Guryanov <sanyaguru@gmail.com>
2024-02-13 17:49:21 +0800

    add seminar4: task1, task2

---
+++ seminar4/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// тип_метода ИмяМетода (пар1, пар2, ... парN)
+// a, b - 2 числа, sign - знак арифм. операции
+int Calculate(int a, int b, string sign)
+{
+    if (sign == "+")
+    {
+        return a + b; // Cумма чисел
+    }
+    else if (sign == "-")
+    {
+        return a - b; // Разность чисел
+    }
+    else if (sign == "*")
+    {
+        return a * b; // Произ-е чисел
+    }
+    else
+    {
+        Console.WriteLine("Введен неизвестный знак арифм. операции");
+        return 0;
+    }
+}
+
+// Вызов метода
+Console.WriteLine(Calculate(2, 3, "+")); // 5
+Console.WriteLine(Calculate(2, 3, "-")); // -1
+Console.WriteLine(Calculate(20, 3, "*")); // 60
+Console.WriteLine(Calculate(20, 3, ".")); // 60
