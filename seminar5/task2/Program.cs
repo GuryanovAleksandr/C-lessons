2ec8cbb5ca8ecf3f2b3bf85a860dc1e0672142bc
Guryanov <sanyaguru@gmail.com>
2024-02-21 16:02:55 +0800

    add seminar4: task2

--- seminar5/task2/Program.cs
+++ seminar5/task2/Program.cs
@@ -1,19 +1,20 @@
-﻿// Задайте двумерный массив. Найдите элементы, у которых оба
-// индекса чётные, и замените эти элементы на их квадраты.
+﻿// Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали (с индексами (0,0); (1; 1) и
+// т.д.
 // Пример
-// 2 3 4 3    4 3 4 3
-// 4 3 4 1 => 4 3 4 1
-// 2 9 5 4    2 9 25 4
+// 2 3 4 3
+//  4 3 4 1 => 2 + 3 + 5 = 10
+//  2 9 5 4
 
-int[,] CreateMatrix(int rowsCount, int colomsCount)
+int[,] CreateMatrix(int rowCount, int colomsCount)
 {
-    int[,] matrix = new int[rowsCount, colomsCount];
+    int[,] matrix = new int[rowCount, colomsCount];
     Random rnd = new Random();
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.Next(101); // от 0 до 100
+            matrix[i, j] = rnd.Next(11);
         }
     }
     return matrix;
@@ -29,27 +30,25 @@ void PrintMatrix(int[,] matrix)
         Console.WriteLine(); //перенос на новую строку
     }
 }
-
-void SquareElements(int[,] matrix)
+int SumElements(int[,] matrix)
 {
+    int sum = 0;
     for (int i = 0; i < matrix.GetLength(0); i++) // строки
     {
         for (int j = 0; j < matrix.GetLength(1); j++) // столбцы
         {
-            if (i % 2 == 0 && j % 2 == 0) // номер строки И номер столбца - четные
+            if (i == j) // номер строки равен номеру столбца
             {
-                matrix[i, j] *= matrix[i, j];// matr[i, j] = matr[i, j] * matr[i, j];
-            } 
+                sum += matrix[i, j];
+            }
         }
     }
+    return sum;
 }
-
 Console.Write("Введите количество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов: ");
 int coloms = Convert.ToInt32(Console.ReadLine());
 int[,] matrix = CreateMatrix(rows, coloms);
 PrintMatrix(matrix);
-Console.WriteLine("Результат:");
-SquareElements(matrix); // Изменяем матрицу
-PrintMatrix(matrix);cd
+Console.WriteLine($"Результат: {SumElements(matrix)}");
